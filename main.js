d1e049b5ac9563ca8f9ee38b0ee6d4198d181886
Bobby <bobbyrobin8@gmail.com>
2020-12-14 22:57:02 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const port = process.env.PORT || 3000;
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat', function(data){
+    console.log(data);
+    socket.emit('chat', data);
+  });
+  //Broadcast typing...
+  // socket.broadcast.emit("");
+});
+
+http.listen(3000, () => {
+  console.log(`listening on port ${port}`);
+});
